bfbaba6e179d664f7afeb094aaef9f54a72add53
martezr <martez.reed@greenreedtech.com>
2021-04-26 22:12:59 -0500

    Update contribution documentation

--- examples/resources/morpheus_network_domain/network_domain.tf
+++
@@ -1,8 +0,0 @@
-resource "morpheus_network_domain" "tf_example_network_domain" {
-  name        = "tfexampledomain"
-  description = "Terraform example network domain"
-  public_zone = true
-  visibility  = "private"
-  tenant_id   = 1
-  active      = true
-}
