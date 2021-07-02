a9242d3fe7046d0549c512c40d96d9939a742fee
martezr <martez.reed@greenreedtech.com>
2021-07-02 09:07:08 -0500

    Add python script task

---
+++ morpheus/resource_python_script_task.go
@@ -0,0 +1,347 @@
+package morpheus
+
+import (
+	"context"
+	"encoding/json"
+	"time"
+
+	"log"
+
+	"github.com/gomorpheus/morpheus-go-sdk"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+)
+
+func resourcePythonScriptTask() *schema.Resource {
+	return &schema.Resource{
+		Description:   "Provides a Morpheus python script task resource",
+		CreateContext: resourcePythonScriptTaskCreate,
+		ReadContext:   resourcePythonScriptTaskRead,
+		UpdateContext: resourcePythonScriptTaskUpdate,
+		DeleteContext: resourcePythonScriptTaskDelete,
+
+		Schema: map[string]*schema.Schema{
+			"id": {
+				Type:        schema.TypeString,
+				Description: "The ID of the python script task",
+				Computed:    true,
+			},
+			"name": {
+				Type:        schema.TypeString,
+				Description: "The name of the python script task",
+				Required:    true,
+			},
+			"code": {
+				Type:        schema.TypeString,
+				Description: "The code of the python script task",
+				Optional:    true,
+			},
+			"result_type": {
+				Type:         schema.TypeString,
+				Description:  "The expected result type (single value, key pairs, json)",
+				ValidateFunc: validation.StringInSlice([]string{"value", "keyValue", "json"}, false),
+				Optional:     true,
+			},
+			"source_type": {
+				Type:         schema.TypeString,
+				Description:  "The source of the python script (local, url or repository)",
+				ValidateFunc: validation.StringInSlice([]string{"local", "url", "repository"}, false),
+				Required:     true,
+			},
+			"script_content": {
+				Type:        schema.TypeString,
+				Description: "The content of the python script. Used when the local source type is specified",
+				Optional:    true,
+			},
+			"script_path": {
+				Type:        schema.TypeString,
+				Description: "The path of the python script, either the url or the path in the repository",
+				Optional:    true,
+			},
+			"repository_id": {
+				Type:        schema.TypeInt,
+				Description: "The ID of the git repository integration",
+				Optional:    true,
+			},
+			"version_ref": {
+				Type:        schema.TypeString,
+				Description: "The git reference of the repository to pull (main, master, etc.)",
+				Optional:    true,
+			},
+			"command_arguments": {
+				Type:        schema.TypeString,
+				Description: "Arguments to pass to the python script",
+				Optional:    true,
+			},
+			"additional_packages": {
+				Type:        schema.TypeString,
+				Description: "Additional python packages to install prior to the execution of the python script",
+				Optional:    true,
+			},
+			"python_binary": {
+				Type:        schema.TypeString,
+				Description: "The system path of the python binary to execute",
+				Optional:    true,
+			},
+			"retryable": {
+				Type:        schema.TypeBool,
+				Description: "Whether to retry the task if there is a failure",
+				Optional:    true,
+			},
+			"retry_count": {
+				Type:        schema.TypeInt,
+				Description: "The number of times to retry the task if there is a failure",
+				Optional:    true,
+				Default:     false,
+			},
+			"retry_delay_seconds": {
+				Type:        schema.TypeInt,
+				Description: "The number of seconds to wait between retry attempts",
+				Optional:    true,
+			},
+			"allow_custom_config": {
+				Type:        schema.TypeBool,
+				Description: "Custom configuration data to pass during the execution of the python script",
+				Optional:    true,
+			},
+		},
+		Importer: &schema.ResourceImporter{
+			StateContext: schema.ImportStatePassthroughContext,
+		},
+	}
+}
+
+func resourcePythonScriptTaskCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	client := meta.(*morpheus.Client)
+
+	// Warning or errors can be collected in a slice type
+	var diags diag.Diagnostics
+
+	name := d.Get("name").(string)
+
+	sourceOptions := make(map[string]interface{})
+	if d.Get("script_content") != "" {
+		sourceOptions["content"] = d.Get("script_content")
+	}
+	if d.Get("script_path") != "" {
+		sourceOptions["contentPath"] = d.Get("script_path")
+	}
+	sourceOptions["contentRef"] = d.Get("version_ref")
+	sourceOptions["repository"] = map[string]interface{}{
+		"id": d.Get("repository_id"),
+	}
+	sourceOptions["sourceType"] = d.Get("source_type")
+
+	taskOptions := make(map[string]interface{})
+	taskOptions["pythonAdditionalPackages"] = d.Get("additional_packages")
+	taskOptions["pythonArgs"] = d.Get("command_arguments")
+	taskOptions["pythonBinary"] = d.Get("python_binary")
+
+	taskType := make(map[string]interface{})
+	taskType["code"] = "jythonTask"
+
+	req := &morpheus.Request{
+		Body: map[string]interface{}{
+			"task": map[string]interface{}{
+				"name":              name,
+				"code":              d.Get("code").(string),
+				"file":              sourceOptions,
+				"taskType":          taskType,
+				"taskOptions":       taskOptions,
+				"resultType":        d.Get("result_type"),
+				"executeTarget":     "local",
+				"retryable":         d.Get("retryable"),
+				"retryCount":        d.Get("retry_count"),
+				"retryDelaySeconds": d.Get("retry_delay_seconds"),
+				"allowCustomConfig": d.Get("allow_custom_config"),
+			},
+		},
+	}
+	resp, err := client.CreateTask(req)
+	if err != nil {
+		log.Printf("API FAILURE: %s - %s", resp, err)
+		return diag.FromErr(err)
+	}
+	log.Printf("API RESPONSE: %s", resp)
+
+	result := resp.Result.(*morpheus.CreateTaskResult)
+	task := result.Task
+	// Successfully created resource, now set id
+	d.SetId(int64ToString(task.ID))
+	log.Printf("Task ID: %s", int64ToString(task.ID))
+
+	resourcePythonScriptTaskRead(ctx, d, meta)
+	return diags
+}
+
+func resourcePythonScriptTaskRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	client := meta.(*morpheus.Client)
+
+	// Warning or errors can be collected in a slice type
+	var diags diag.Diagnostics
+
+	id := d.Id()
+	name := d.Get("name").(string)
+
+	// lookup by name if we do not have an id yet
+	var resp *morpheus.Response
+	var err error
+	if id == "" && name != "" {
+		resp, err = client.FindTaskByName(name)
+	} else if id != "" {
+		resp, err = client.GetTask(toInt64(id), &morpheus.Request{})
+	} else {
+		return diag.Errorf("Task cannot be read without name or id")
+	}
+
+	if err != nil {
+		// 404 is ok?
+		if resp != nil && resp.StatusCode == 404 {
+			log.Printf("API 404: %s - %s", resp, err)
+			return diag.FromErr(err)
+		} else {
+			log.Printf("API FAILURE: %s - %s", resp, err)
+			return diag.FromErr(err)
+		}
+	}
+	log.Printf("API RESPONSE: %s", resp)
+
+	// store resource data
+	var pythonScriptTask PythonScript
+	json.Unmarshal(resp.Body, &pythonScriptTask)
+	d.SetId(intToString(pythonScriptTask.Task.ID))
+	d.Set("name", pythonScriptTask.Task.Name)
+	d.Set("code", pythonScriptTask.Task.Code)
+	d.Set("result_type", pythonScriptTask.Task.Resulttype)
+	d.Set("source_type", pythonScriptTask.Task.File.Sourcetype)
+	d.Set("script_content", pythonScriptTask.Task.File.Content)
+	d.Set("script_path", pythonScriptTask.Task.File.Contentpath)
+	d.Set("version_ref", pythonScriptTask.Task.File.Contentref)
+	d.Set("repository_id", pythonScriptTask.Task.File.Repository.ID)
+	d.Set("retryable", pythonScriptTask.Task.Retryable)
+	d.Set("retry_count", pythonScriptTask.Task.Retrycount)
+	d.Set("retry_delay_seconds", pythonScriptTask.Task.Retrydelayseconds)
+	d.Set("allow_custom_config", pythonScriptTask.Task.Allowcustomconfig)
+	return diags
+}
+
+func resourcePythonScriptTaskUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	client := meta.(*morpheus.Client)
+	id := d.Id()
+	name := d.Get("name").(string)
+
+	sourceOptions := make(map[string]interface{})
+	if d.Get("script_content") != "" {
+		sourceOptions["content"] = d.Get("script_content")
+	}
+	if d.Get("script_path") != "" {
+		sourceOptions["contentPath"] = d.Get("script_path")
+	}
+	sourceOptions["contentRef"] = d.Get("version_ref")
+	sourceOptions["repository"] = map[string]interface{}{
+		"id": d.Get("repository_id"),
+	}
+	sourceOptions["sourceType"] = d.Get("source_type")
+
+	taskOptions := make(map[string]interface{})
+	taskOptions["pythonAdditionalPackages"] = d.Get("additional_packages")
+	taskOptions["pythonArgs"] = d.Get("command_arguments")
+	taskOptions["pythonBinary"] = d.Get("python_binary")
+
+	taskType := make(map[string]interface{})
+	taskType["code"] = "jythonTask"
+
+	req := &morpheus.Request{
+		Body: map[string]interface{}{
+			"task": map[string]interface{}{
+				"name":              name,
+				"code":              d.Get("code").(string),
+				"file":              sourceOptions,
+				"taskType":          taskType,
+				"taskOptions":       taskOptions,
+				"resultType":        d.Get("result_type"),
+				"executeTarget":     "local",
+				"retryable":         d.Get("retryable"),
+				"retryCount":        d.Get("retry_count"),
+				"retryDelaySeconds": d.Get("retry_delay_seconds"),
+				"allowCustomConfig": d.Get("allow_custom_config"),
+			},
+		},
+	}
+	log.Printf("API REQUEST: %s", req)
+	resp, err := client.UpdateTask(toInt64(id), req)
+	if err != nil {
+		log.Printf("API FAILURE: %s - %s", resp, err)
+		return diag.FromErr(err)
+	}
+	log.Printf("API RESPONSE: %s", resp)
+	result := resp.Result.(*morpheus.UpdateTaskResult)
+	pythonScriptTask := result.Task
+	// Successfully updated resource, now set id
+	// err, it should not have changed though..
+	d.SetId(int64ToString(pythonScriptTask.ID))
+	return resourcePythonScriptTaskRead(ctx, d, meta)
+}
+
+func resourcePythonScriptTaskDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	client := meta.(*morpheus.Client)
+
+	// Warning or errors can be collected in a slice type
+	var diags diag.Diagnostics
+
+	id := d.Id()
+	req := &morpheus.Request{}
+	resp, err := client.DeleteTask(toInt64(id), req)
+	if err != nil {
+		if resp != nil && resp.StatusCode == 404 {
+			log.Printf("API 404: %s - %s", resp, err)
+			return nil
+		} else {
+			log.Printf("API FAILURE: %s - %s", resp, err)
+			return diag.FromErr(err)
+		}
+	}
+	log.Printf("API RESPONSE: %s", resp)
+	d.SetId("")
+	return diags
+}
+
+type PythonScript struct {
+	Task struct {
+		ID        int    `json:"id"`
+		Accountid int    `json:"accountId"`
+		Name      string `json:"name"`
+		Code      string `json:"code"`
+		Tasktype  struct {
+			ID   int    `json:"id"`
+			Code string `json:"code"`
+			Name string `json:"name"`
+		} `json:"taskType"`
+		Taskoptions struct {
+			Pythonbinary             string      `json:"pythonBinary"`
+			Pythonargs               string      `json:"pythonArgs"`
+			Pythonadditionalpackages string      `json:"pythonAdditionalPackages"`
+			Pythonscript             interface{} `json:"pythonScript"`
+		} `json:"taskOptions"`
+		File struct {
+			ID          int    `json:"id"`
+			Sourcetype  string `json:"sourceType"`
+			Contentref  string `json:"contentRef"`
+			Contentpath string `json:"contentPath"`
+			Repository  struct {
+				ID   int    `json:"id"`
+				Name string `json:"name"`
+			} `json:"repository"`
+			Content interface{} `json:"content"`
+		} `json:"file"`
+		Resulttype        string    `json:"resultType"`
+		Executetarget     string    `json:"executeTarget"`
+		Retryable         bool      `json:"retryable"`
+		Retrycount        int       `json:"retryCount"`
+		Retrydelayseconds int       `json:"retryDelaySeconds"`
+		Allowcustomconfig bool      `json:"allowCustomConfig"`
+		Datecreated       time.Time `json:"dateCreated"`
+		Lastupdated       time.Time `json:"lastUpdated"`
+	} `json:"task"`
+}
