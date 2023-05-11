a9f40e49bc37d5aaae9905e927cf544c9ef5103c
Martez Reed <martez.reed@gmail.com>
2023-05-10 20:25:37 -0500

    Add license resource support (#117)
    * Add license resource support
    
    * Added virtual image type (#110)
    
    * Added datafield for virtual image type and changed if statement to enable virtual image datatype usage. I commited a change to the SDK as well that is needed for this change to work
    
    * Added resource_key_pair to be able to manage Key Pairs, did minor changes in the key pair datasource
    
    * Fix task job read and add support for labels as well as additional context types (#118)
    
    * Fix merge conflict with virtual image commit
    
    ---------
    
    Co-authored-by: Stefan Berner <114592275+StefanBerner@users.noreply.github.com>

---
+++ morpheus/resource_license.go
@@ -0,0 +1,141 @@
+package morpheus
+
+import (
+	"context"
+
+	"log"
+
+	"github.com/gomorpheus/morpheus-go-sdk"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func resourceLicense() *schema.Resource {
+	return &schema.Resource{
+		Description:   "Provides a Morpheus license resource.",
+		CreateContext: resourceLicenseCreate,
+		ReadContext:   resourceLicenseRead,
+		UpdateContext: resourceLicenseUpdate,
+		DeleteContext: resourceLicenseDelete,
+
+		Schema: map[string]*schema.Schema{
+			"id": {
+				Type:        schema.TypeString,
+				Description: "The ID of the license",
+				Computed:    true,
+			},
+			"key": {
+				Type:        schema.TypeString,
+				Description: "The Morpheus license key",
+				Required:    true,
+				Sensitive:   true,
+			},
+		},
+		Importer: &schema.ResourceImporter{
+			StateContext: schema.ImportStatePassthroughContext,
+		},
+	}
+}
+
+func resourceLicenseCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	client := meta.(*morpheus.Client)
+
+	// Warning or errors can be collected in a slice type
+	var diags diag.Diagnostics
+
+	req := &morpheus.Request{
+		Body: map[string]interface{}{
+			"license": d.Get("key").(string),
+		},
+	}
+
+	resp, err := client.InstallLicense(req)
+	if err != nil {
+		log.Printf("API FAILURE: %s - %s", resp, err)
+		return diag.FromErr(err)
+	}
+	log.Printf("API RESPONSE: %s", resp)
+
+	result := resp.Result.(*morpheus.GetLicenseResult)
+	_ = result.License
+	// Successfully created resource, now set id
+	d.SetId(int64ToString(1))
+
+	resourceLicenseRead(ctx, d, meta)
+	return diags
+}
+
+func resourceLicenseRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	client := meta.(*morpheus.Client)
+	// Warning or errors can be collected in a slice type
+	var diags diag.Diagnostics
+
+	// lookup by name if we do not have an id yet
+	var resp *morpheus.Response
+	var err error
+
+	resp, err = client.GetLicense(&morpheus.Request{})
+	if err != nil {
+		if resp != nil && resp.StatusCode == 404 {
+			log.Printf("API 404: %s - %s", resp, err)
+			return diag.FromErr(err)
+		} else {
+			log.Printf("API FAILURE: %s - %s", resp, err)
+			return diag.FromErr(err)
+		}
+	}
+	log.Printf("API RESPONSE: %s", resp)
+
+	// store resource data
+	result := resp.Result.(*morpheus.GetLicenseResult)
+	_ = result.License
+	d.SetId(int64ToString(1))
+
+	return diags
+}
+
+func resourceLicenseUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	client := meta.(*morpheus.Client)
+
+	req := &morpheus.Request{
+		Body: map[string]interface{}{
+			"license": d.Get("key").(string),
+		},
+	}
+
+	log.Printf("API REQUEST: %s", req)
+	resp, err := client.InstallLicense(req)
+	if err != nil {
+		log.Printf("API FAILURE: %s - %s", resp, err)
+		return diag.FromErr(err)
+	}
+	log.Printf("API RESPONSE: %s", resp)
+	result := resp.Result.(*morpheus.GetLicenseResult)
+	_ = result.License
+	// Successfully created resource, now set id
+	d.SetId(int64ToString(1))
+
+	return resourceLicenseRead(ctx, d, meta)
+}
+
+func resourceLicenseDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	client := meta.(*morpheus.Client)
+
+	// Warning or errors can be collected in a slice type
+	var diags diag.Diagnostics
+
+	req := &morpheus.Request{}
+	resp, err := client.UninstallLicense(1, req)
+	if err != nil {
+		if resp != nil && resp.StatusCode == 404 {
+			log.Printf("API 404: %s - %s", resp, err)
+			return diag.FromErr(err)
+		} else {
+			log.Printf("API FAILURE: %s - %s", resp, err)
+			return diag.FromErr(err)
+		}
+	}
+	log.Printf("API RESPONSE: %s", resp)
+	d.SetId("")
+	return diags
+}
