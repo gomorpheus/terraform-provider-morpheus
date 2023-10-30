cd244bcfe532c990ed73178b59dc12565c1aef2f
Ollie Phillips <ollie@interject.io>
2023-10-30 16:23:48 +0000

    adds labels support to script template and file template (#171)

--- examples/resources/morpheus_script_template/resource.tf
+++ examples/resources/morpheus_script_template/resource.tf
@@ -1,5 +1,6 @@
 resource "morpheus_script_template" "tfexample_script_template" {
   name           = "tf-terraform-script-template"
+  labels         = ["demo", "template", "terraform"]
   script_type    = "bash"
   script_phase   = "provision"
   script_content = <<EOF
